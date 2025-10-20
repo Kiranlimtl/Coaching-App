5238a580b5204c66b3d58af6453d40c0e8a82bc1
Kiranlimtl <kiranlimtl@gmail.com>
2025-10-20 19:19:27 +0800

    Login page half done

--- CoachingAppFrontend/CoachingApp/src/app/_layout.tsx
+++ CoachingAppFrontend/CoachingApp/src/app/_layout.tsx
@@ -1,23 +1,5 @@
-import { Stack, router } from "expo-router";
-import { Text } from "react-native";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="createUpdateReminder"
-          options={{
-            presentation: 'modal',
-            headerTitle: 'New Reminder',
-            headerLeft: () => <Text style={{ color: '#0E7AFE' }} onPress={() => router.back()}>Cancel</Text>
-          }}
-        />
-      </Stack>
-    </QueryClientProvider>
-  )
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
